e9f13e42c8f5d3348f56f8a08be95a269a9d843b
Klaiveft <swan1.dagnas@hmail.com>
2026-03-26 10:13:35 +0100

    CSS et responsive

--- src/Core/SQLDatabase.php
+++ src/Core/SQLDatabase.php
@@ -20,17 +20,7 @@ class SQLDatabase implements Database {
         $user = 'root';
         $pass = '';
         $port = 3307;
-    
-        try {
-            $this->database = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $pass);
-            
-            // Activer les exceptions et le mode de retour en tableau associatif par défaut
-            $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->database->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            
-        } catch (PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
-        }
+
     }
 
     /**
@@ -38,8 +28,7 @@ class SQLDatabase implements Database {
      */
     public function getAllCompany()
     {
-        $stmt = $this->database->query("SELECT * FROM company");
-        $liste = $stmt->fetchAll();
+        $liste = 0;
         return $liste;
     }
 
