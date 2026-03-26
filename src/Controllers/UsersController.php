782719388e45cb829b0434c9576b13205b82e066
Kaëlig <162746512+KaeligCl@users.noreply.github.com>
2026-03-26 10:17:54 +0100

    style pour le changement de mdp

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -32,6 +32,15 @@ class UsersController extends Controller {
 
         $userInfo = $this->UsersModel->getUserInfo($_SESSION['user_id']);
 
+        $passwordMessage = null;
+        if (isset($_GET['status'])) {
+            if ($_GET['status'] == 'success'){
+                $passwordMessage = ['type' => 'success', 'text' => 'Mot de passe mis à jour !'];
+            } else {
+                $passwordMessage = ['type' => 'error', 'text' => 'Mot de passe non identique'];
+            }
+        }
+
         $mois = ["", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
         $date_brute = new DateTime($userInfo['date_login']);
         $num_mois = $date_brute->format('n'); // Récupère le numéro du mois sans le zéro
@@ -40,12 +49,13 @@ class UsersController extends Controller {
 
         $action = $_GET['action'] ?? null;
         echo $this->templateEngine->render('common/MyAccount.twig.html', [
-            'nav'          => $this->Dashboard(),
-            'userInfo'     => $userInfo,
-            'editPassword' => ($action === 'editPassword'), // Vrai si ?action=editPassword
-            'stats'        => ['applications' => 5, 'favorites' => 1, 'saved_offers' => 2],
-            'activities'   => [],
-            'last_login_fr'  => $date_fr
+            'nav'               => $this->Dashboard(),
+            'userInfo'          => $userInfo,
+            'editPassword'      => ($action === 'editPassword'), // Vrai si ?action=editPassword
+            'passwordMessage'   => $passwordMessage,
+            'stats'             => ['applications' => 5, 'favorites' => 1, 'saved_offers' => 2],
+            'activities'        => [],
+            'last_login_fr'     => $date_fr
         ]);
     }
 
