5d87ec0b33d485ea56f29af91fcf4f3f034fa6f1
Moizant <amoizant056@gmail.com>
2026-03-27 14:04:31 +0100

    Amélioration pagination

--- src/Models/SearchModel.php
+++ src/Models/SearchModel.php
@@ -19,6 +19,7 @@ class SearchModel extends Model {
         return $this->database->getAllCompany();
     }
     
+<<<<<<< HEAD
    public function getCompaniesPaginated($limit, $offset) {
     $companies = $this->database->getAllCompany();
 
@@ -32,6 +33,18 @@ public function countCompanies() {
 }
     
 
+=======
+    public function getCompaniesPaginated($limit, $offset) {
+    $db = Database::getConnection();
+
+    $stmt = $db->prepare("SELECT * FROM company LIMIT :limit OFFSET :offset");
+    $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+    $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+
+    $stmt->execute();
+    return $stmt->fetchAll();
+}
+>>>>>>> 9b2b6e7 (Ajout de la récupération paginée des entreprises)
 }
 
 ?>
